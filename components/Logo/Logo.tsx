50a8531f64b1f1fdffe4d4a2affc41c14b4292e4
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-18 16:00:56 +0200

    add logo icon, fill for hover and active

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import Link from 'next/link';
+import LogoIcon from '../../public/icons/logo.svg';
 
 type ILogo = {
   className?: string;
@@ -7,8 +8,11 @@ type ILogo = {
 
 export const Logo = ({ className = '' }: ILogo) => {
   return (
-    <div className={classNames('container', className)}>
-      <Link href="/"></Link>
-    </div>
+    <Link
+      href="/"
+      className={classNames('cursor-pointer inline-block', className)}
+    >
+      <LogoIcon className="w-[120px] h-[15px] md:w-[161px] md:h-[20px] fill-accent active:fill-accentPressed xl:hover:fill-accentHover" />
+    </Link>
   );
 };
