e57d4b47dea471b9876e24408bf158db6ef50f7a
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-18 16:34:03 +0200

    add aria-label

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -11,6 +11,7 @@ export const Logo = ({ className = '' }: ILogo) => {
     <Link
       href="/"
       className={classNames('cursor-pointer inline-block', className)}
+      aria-label="Icon with name and surname for link to Homepage"
     >
       <LogoIcon className="w-[120px] h-[15px] md:w-[161px] md:h-[20px] fill-accent active:fill-accentPressed xl:hover:fill-accentHover" />
     </Link>
