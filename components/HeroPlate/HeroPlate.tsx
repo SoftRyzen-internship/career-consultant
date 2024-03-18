daa03cee15ba3ae62ac7bc284101169f777bdc8a
chemvic <chemvic79@gmail.com>
2024-03-18 15:00:40 +0200

    upd plate

--- components/HeroPlate/HeroPlate.tsx
+++ components/HeroPlate/HeroPlate.tsx
@@ -7,7 +7,7 @@ type HeroPlateProps = {
 
 const HeroPlate: React.FC<HeroPlateProps> = ({ quantity, description }) => {
   return (
-    <div className="w-[162px] h-[124px] border-0 rounded-lg shadow-custom bg-white py-3 px-4 sm:w-[222px] sm:h-[106px] md:p-5 md:w-[160px] md:h-[141px] xl:py-6 xl:px-8 ">
+    <div className="w-[162px] h-[124px] border-0 rounded-lg shadow-custom bg-white py-[12px] px-[16px] sm:w-[222px] sm:h-[106px] md:p-5 md:w-[160px] md:h-[141px] xl:py-[24px] xl:px-[32px] ">
       <p className="text-text01 font-fixel-medium text-[32px] leading-[1.21] ">
         {quantity}
       </p>
