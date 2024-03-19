9a45104a20e9b3c09ca88a62958e3990aa1d187d
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-19 12:15:51 +0200

    add aria-labels, width for tablet, change hover-color

--- components/Socials/Socials.tsx
+++ components/Socials/Socials.tsx
@@ -1,17 +1,18 @@
 import Link from 'next/link';
 
-import EmailIcon from '../../public/icons/email.svg';
-import TelegramIcon from '../../public/icons/telegram.svg';
-import common from '../../data/common.json';
 import classNames from 'classnames';
 
-const { email, telegram } = common;
+import EmailIcon from '../../public/icons/email.svg';
+import TelegramIcon from '../../public/icons/telegram.svg';
+
+import common from '../../data/common.json';
 
 type ISocials = {
   className?: string;
 };
 
 export const Socials = ({ className = '' }: ISocials) => {
+  const { email, telegram } = common;
   return (
     <ul
       className={classNames('text-sm xl:text-base xl:font-normal', className)}
@@ -21,10 +22,10 @@ export const Socials = ({ className = '' }: ISocials) => {
           href={`mailto:${email}`}
           target="_blank"
           rel="noopener noreferrer nofollow"
-          className="py-2 xl:py-3 inline-flex gap-4 items-center text-accent active:text-accentPressed xl:hover:text-accentHover"
+          className="py-2 xl:py-3 inline-flex gap-4 items-center text-accent active:text-text02 xl:hover:text-text02 xl:focus:text-text02"
         >
-          <EmailIcon className="w-6 h-6 stroke-accent" />
-          <span className="text-text01 active:text-accentPressed xl:hover:text-accentHover">
+          <EmailIcon className="w-6 h-6 stroke-accent" aria-label="Envelope" />
+          <span className="md:w-[148px] break-all xl:w-full text-text01 active:text-text02 xl:hover:text-text02 xl:focus:text-text02">
             {email}
           </span>
         </Link>
@@ -34,10 +35,10 @@ export const Socials = ({ className = '' }: ISocials) => {
           href={`https://t.me/${telegram.substring(1)}`}
           target="_blank"
           rel="noopener noreferrer nofollow"
-          className="py-2 xl:py-3 inline-flex gap-4 items-center text-accent"
+          className="py-2 xl:py-3 inline-flex gap-4 items-center text-accent active:text-text02 xl:hover:text-text02 xl:focus:text-text02"
         >
-          <TelegramIcon className="w-6 h-6" />
-          <span className="text-text01 active:text-accentPressed xl:hover:text-accentHover">
+          <TelegramIcon className="w-6 h-6" aria-label="Paper airplane" />
+          <span className="text-text01 active:text-text02 xl:hover:text-text02 xl:focus:text-text02">
             {telegram}
           </span>
         </Link>
