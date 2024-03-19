f4ef3ce43fd31c7e65ff62c38700cad6a776df6e
Stepan Skichko <st.skichko@gmail.com>
2024-03-19 14:42:49 +0200

    add NavBar

--- app/(site)/test/page.tsx
+++ app/(site)/test/page.tsx
@@ -1,22 +1,35 @@
 'use client';
 
-import { Button } from '@/components/Button';
+// import { Button } from '@/components/Button';
+import { NavBar } from '@/components/NavBar/NavBar';
+import { Element } from 'react-scroll';
 
 const page = () => {
   return (
     <div className="max-w-[300px] w-full mx-auto">
       <p className="mb-[20px]">test page</p>
-      <Button
+      {/* <Button
         type="submit"
         onClick={() => {
           console.log('hi');
         }}
-        isSubmitted={false}
+        isSubmitted={true}
         isSubmitError={false}
         disabled={false}
       >
         Read more
-      </Button>
+      </Button> */}
+
+      <NavBar />
+      <Element name="test1" className="requestSection">
+        <div className="mt-[50px] h-[1000px] bg-green-500">requestSection</div>
+      </Element>
+      <Element name="target" className="reviewsSection">
+        <div className="h-[1000px] bg-red-400">reviewsSection</div>
+      </Element>
+      <Element name="target" className="feedbackSection">
+        <div className="h-[1000px] bg-blue-600">feedbackSection</div>
+      </Element>
     </div>
   );
 };
