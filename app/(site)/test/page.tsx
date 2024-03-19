5abc70fd6f95120437f72b30e6c2ff2b91ee1927
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-19 13:25:09 +0200

    add component Socials to test page

--- app/(site)/test/page.tsx
+++ app/(site)/test/page.tsx
@@ -1,22 +1,27 @@
 'use client';
 
-import { Button } from '@/components/Button';
+// import { Button } from '@/components/Button';
+import { Socials } from '@/components/Socials';
 
 const page = () => {
   return (
-    <div className="max-w-[300px] w-full mx-auto">
+    <div className="container w-full mx-auto">
       <p className="mb-[20px]">test page</p>
-      <Button
-        type="submit"
-        onClick={() => {
-          console.log('hi');
-        }}
-        isSubmitted={false}
-        isSubmitError={false}
-        disabled={false}
-      >
-        Read more
-      </Button>
+      {/* <div className="max-w-[300px] w-full mx-auto">
+        <p className="mb-[20px]">test page</p>
+        <Button
+          type="submit"
+          onClick={() => {
+            console.log('hi');
+          }}
+          isSubmitted={false}
+          isSubmitError={false}
+          disabled={false}
+        >
+          Read more
+        </Button>
+      </div> */}
+      <Socials />
     </div>
   );
 };
