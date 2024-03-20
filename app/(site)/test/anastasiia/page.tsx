f208e2906fe9572a8c4047a96ec5e7a2030a77ed
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-20 19:25:13 +0200

    create component RequesCard, json for it, add path /test/anastasiia

---
+++ app/(site)/test/anastasiia/page.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import { Button } from '@/components/Button';
+import { NavBar } from '@/components/NavBar/NavBar';
+import { Element } from 'react-scroll';
+import { Socials } from '@/components/Socials';
+import { LinkTelegram } from '@/components/LinkTelegram';
+import { LinkToFeedback } from '@/components/LinkToFeedback';
+
+import requests from '../../../../data/requests.json';
+import { RequestCard } from '@/components/RequestCard';
+
+const page = () => {
+  return (
+    <>
+      <div className="max-w-[300px] w-full mx-auto">
+        <p className="mb-[20px]">test page</p>
+        <Button
+          type="submit"
+          onClick={() => {
+            console.log('hi');
+          }}
+          isSubmitted={false}
+          isSubmitError={false}
+          disabled={false}
+        >
+          Read more
+        </Button>
+
+        <NavBar />
+        <Element name="test1" className="requestSection">
+          <div className="mt-[50px] h-[1000px] bg-green-500">
+            requestSection
+          </div>
+        </Element>
+        <Element name="target" className="reviewsSection">
+          <div className="h-[1000px] bg-red-400">reviewsSection</div>
+        </Element>
+        <Element name="target" className="feedbackSection">
+          <div className="h-[1000px] bg-blue-600">feedbackSection</div>
+        </Element>
+        <Element name="target" className="feedback">
+          <div className="h-[1000px] bg-orange-600 text-white text-base">
+            Feedback Section with Form
+          </div>
+        </Element>
+      </div>
+
+      <Socials />
+      <div className="mb-5"></div>
+      <div className="px-3">
+        <LinkTelegram />
+        <div className="mb-5"></div>
+        <LinkToFeedback section="header" />
+        <div className="mb-5"></div>
+        <LinkToFeedback section="hero" />
+        <div className="mb-5"></div>
+        <LinkToFeedback section="services" />
+        <div className="mb-5"></div>
+        <LinkToFeedback section="mobileMenu" />
+        <div className="mb-5"></div>
+        <ul>
+          {requests.map(request => {
+            return (
+              <RequestCard
+                key={request.id}
+                request={request}
+                className="mb-5"
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default page;
