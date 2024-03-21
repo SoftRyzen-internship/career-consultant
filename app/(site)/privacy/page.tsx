5ea3b6d9bfc72631d83ccc27f2f2e62a76814cf6
chemvic <chemvic79@gmail.com>
2024-03-22 01:19:48 +0200

    footer

---
+++ app/(site)/privacy/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@/components/Container';
+
+const page = () => {
+  return (
+    <>
+      <Container>
+        <h1 className="text-center text-[50px] font-semibold">Privacy Page</h1>
+      </Container>
+    </>
+  );
+};
+export default page;
---
+++ layout/Footer/types.ts
@@ -0,0 +1,11 @@
+export type Foot = {
+  privacy: string;
+  home: string;
+  created: string;
+  links: Links;
+};
+
+type Links = {
+  goit: string;
+  ryzen: string;
+};
