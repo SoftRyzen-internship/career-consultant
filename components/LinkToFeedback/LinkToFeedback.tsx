93f5e105edb6dc808654bd8028810bae1b56cde4
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-22 11:32:05 +0200

    change conditional styles in LinkToFeedback

--- components/LinkToFeedback/LinkToFeedback.tsx
+++ components/LinkToFeedback/LinkToFeedback.tsx
@@ -10,28 +10,21 @@ type ILinkToFeedback = {
   className?: string;
 };
 
-export const LinkToFeedback = ({
-  section,
-  className = '',
-}: ILinkToFeedback) => {
+export const LinkToFeedback = ({ section }: ILinkToFeedback) => {
   const { order, leaveApplication, moveToFeedback } = common;
 
-  const sectionStyles = {
-    header: 'px-5 py-3 max-w-[199px] hidden xl:flex',
-    hero: 'px-8 py-4 xl:py-5 md:max-w-[336px] xl:max-w-[280px]',
-    services: 'px-5 py-3 max-w-[128px]',
-    mobileMenu: 'px-8 py-4 sm:max-w-[456px] md:max-w-[250px]',
-  };
-
-  const sectionClass = sectionStyles[section] || '';
+  const className = classNames({
+    'px-5 py-3 max-w-[199px] hidden xl:flex': section === 'header',
+    'px-8 py-4 xl:py-5 md:max-w-[336px] xl:max-w-[280px]': section === 'hero',
+    'px-5 py-3 max-w-[128px]': section === 'services',
+    'px-8 py-4 sm:max-w-[456px] md:max-w-[250px]': section === 'mobileMenu',
+    'cursor-pointer flex items-center justify-center w-full shadow-custom bg-accent active:bg-accentPressed xl:hover:bg-accentHover xl:focus:bg-accentHover font-mulish text-white rounded-[20px] text-base leading-[1.3] transition-all duration-300 ':
+      true,
+  });
 
   return (
     <Link
-      className={classNames(
-        'cursor-pointer flex items-center justify-center w-full shadow-custom bg-accent active:bg-accentPressed xl:hover:bg-accentHover xl:focus:bg-accentHover font-mulish text-white rounded-[20px] text-base leading-[1.3] transition-all duration-300 ',
-        sectionClass,
-        className,
-      )}
+      className={className}
       to={moveToFeedback}
       smooth={true}
       offset={-50}
