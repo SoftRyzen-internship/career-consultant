a03a3e0d82a0b2cfa3d3fbe68b8d3b1d00771235
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-23 23:57:32 +0200

    styles for bg-image

--- sections/WhatChoose/WhatChoose.tsx
+++ sections/WhatChoose/WhatChoose.tsx
@@ -8,9 +8,7 @@ import { LinkTelegram } from '@/components/LinkTelegram';
 import data from '@/data/whatChoose.json';
 
 export type IWhatChoose = {
-  // children: ReactNode;
   isGeneralSection: boolean;
-  isChooseOrFeedbackSection?: boolean;
   className?: string;
 };
 
@@ -18,18 +16,17 @@ export const WhatChoose = ({ className = '' }: IWhatChoose) => {
   const { title, text } = data;
   return (
     <Section
-      className={classNames('choose pb-7', className)}
+      className={classNames('choose', className)}
       isGeneralSection={false}
-      isChooseOrFeedbackSection={false}
     >
       <Container>
-        <div className="relative md:static top-[384px] py-7 md:py-10 xl:py-20">
+        <div className="pb-7 pt-[412px] md:py-10 xl:py-20">
           <SectionTitle
             text={title}
             center={true}
             className="mb-4 md:mb-6 xl:mb-7 md:text-left md:w-[248px] xl:w-[292px]"
           />
-          <p className="mb-10 md:mb-[84px] xl:mb-[54px] w-[284px] mx-auto md:ml-0 font-mulish text-sm xl:text-base xl:font-normal  text-center md:text-left md:w-[248px] xl:w-[292px]">
+          <p className="mb-10 md:mb-[84px] xl:mb-[54px] w-[284px] mx-auto md:ml-0 font-mulish text-text02 text-sm xl:text-base xl:font-normal  text-center md:text-left md:w-[248px] xl:w-[292px]">
             {text}
           </p>
           <LinkTelegram />
