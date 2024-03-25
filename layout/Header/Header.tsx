272565daed00691af33d81671d3075fab7561e5c
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-25 16:01:16 +0200

    add burger menu

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import { useState } from 'react';
+
 import { Container } from '@/components/Container';
 import { Logo } from '@/components/Logo';
 import { NavBar } from '@/components/NavBar';
+import { BurgerMenu } from '@/components/BurgerMenu';
 import { LinkToFeedback } from '@/components/LinkToFeedback';
 
 import Burger from '@/public/icons/burger.svg';
@@ -14,6 +17,9 @@ type IHeader = {
 };
 
 export const Header = ({ className }: IHeader) => {
+  const [isBurgerOpen, setIsBurgerOpen] = useState(false);
+  const onClose = () => setIsBurgerOpen(false);
+
   return (
     <header className={className}>
       <Container className="h-full flex items-center justify-between">
@@ -22,12 +28,17 @@ export const Header = ({ className }: IHeader) => {
           <NavBar />
           <LinkToFeedback section="header" className="" />
         </div>
-        <button className="xl:hidden text-accent active:text-accentPressed ">
+        <button
+          type="button"
+          onClick={() => setIsBurgerOpen(true)}
+          className="xl:hidden text-accent active:text-accentPressed "
+        >
           <Burger
             className="w-6 h-6 mr-2 md:mr-0"
             aria-label={ariaLabelBurger}
           />
         </button>
+        <BurgerMenu isOpen={isBurgerOpen} onClose={onClose} />
       </Container>
     </header>
   );
