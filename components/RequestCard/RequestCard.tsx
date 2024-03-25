77691db653071057b50f58a07b484560544488ba
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-25 12:20:49 +0200

    change json, add alias

--- components/RequestCard/RequestCard.tsx
+++ components/RequestCard/RequestCard.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 
-import { CardTitle } from '../CardTitle';
+import { CardTitle } from '@/components/CardTitle';
 
 import classNames from 'classnames';
 
