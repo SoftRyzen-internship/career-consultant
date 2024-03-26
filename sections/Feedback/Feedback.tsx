2a0e551a3aa28be2301029c24088c31400cb8814
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-26 18:15:29 +0200

    finish styles

--- sections/Feedback/Feedback.tsx
+++ sections/Feedback/Feedback.tsx
@@ -19,17 +19,17 @@ export const Feedback = ({ className = '' }: IFeedback) => {
       className={classNames('bg-white', className)}
       isChooseOrFeedbackSection
     >
-      <Container>
-        <div className="">
+      <Container className="md:flex justify-between">
+        <div>
           <SectionTitle
             text={title}
             center={true}
             className="mb-4 md:mb-6 xl:mb-7 md:text-left md:w-[248px] xl:w-[292px]"
           />
-          <p className="mb-6 md:mb-[60px] xl:mb-[52px] w-[260px] mx-auto font-mulish text-text02 text-sm xl:text-base xl:font-normal text-center md:text-left md:w-[248px] xl:w-[292px]">
+          <p className="mb-6 md:mb-[60px] xl:mb-[52px] w-[260px] mx-auto md:ml-0 font-mulish text-text02 text-sm xl:text-base xl:font-normal text-center md:text-left md:w-[248px] xl:w-[280px]">
             {text}
           </p>
-          <Socials className="w-[279px] mx-auto" />
+          <Socials className="w-[279px] mx-auto xl:w-[308px]" />
         </div>
         <Form />
       </Container>
