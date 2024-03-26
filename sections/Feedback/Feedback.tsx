87c6f5a3ff4db119d68966cee9fc787df299c6d7
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-26 18:22:16 +0200

    fix width, delete empty classname

--- sections/Feedback/Feedback.tsx
+++ sections/Feedback/Feedback.tsx
@@ -24,7 +24,7 @@ export const Feedback = ({ className = '' }: IFeedback) => {
           <SectionTitle
             text={title}
             center={true}
-            className="mb-4 md:mb-6 xl:mb-7 md:text-left md:w-[248px] xl:w-[292px]"
+            className="mb-4 md:mb-6 xl:mb-7 md:text-left md:w-[248px] xl:w-[280px]"
           />
           <p className="mb-6 md:mb-[60px] xl:mb-[52px] w-[260px] mx-auto md:ml-0 font-mulish text-text02 text-sm xl:text-base xl:font-normal text-center md:text-left md:w-[248px] xl:w-[280px]">
             {text}
