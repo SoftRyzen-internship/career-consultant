dbd7fc3f6c5f85143aaea816fd6f1a2c3ee9712e
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-27 12:19:40 +0200

    add pagging for desktop

--- sections/Feedback/Feedback.tsx
+++ sections/Feedback/Feedback.tsx
@@ -20,11 +20,11 @@ export const Feedback = ({ className = '' }: IFeedback) => {
       isChooseOrFeedbackSection
     >
       <Container className="md:flex justify-between">
-        <div>
+        <div className="xl:pt-[26px]">
           <SectionTitle
             text={title}
             center={true}
-            className="mb-4 md:mb-8 xl:mb-14 md:text-left md:w-[248px] xl:w-[280px]"
+            className="mb-4 md:mb-8 xl:mb-[30px] md:text-left md:w-[248px] xl:w-[280px]"
           />
           <p className="mb-6 md:mb-[80px] xl:mb-[52px] w-[260px] mx-auto md:ml-0 font-mulish text-text02 text-sm xl:text-base xl:font-normal text-center md:text-left md:w-[248px] xl:w-[280px]">
             {text}
