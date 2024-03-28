b6bc07d64e9375fc49c89e2d71b3024b2f4c1d3c
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2024-03-28 03:32:18 +0200

    add neccessary styles for footer link

--- components/FooterLink/FooterLink.tsx
+++ components/FooterLink/FooterLink.tsx
@@ -10,7 +10,10 @@ export const FooterLink = () => {
   const { home, policy } = footer;
 
   return (
-    <Link href={pathname === '/' ? '/policy' : '/'}>
+    <Link
+      className="font-mulish text-xs text-text02 xl:text-[16px] font-medium leading-[1.4] relative policy  transition-all duration-300 hover:text-text01  focus:text-text01 "
+      href={pathname === '/' ? '/policy' : '/'}
+    >
       {pathname === '/' ? policy : home}
     </Link>
   );
