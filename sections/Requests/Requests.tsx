3a2ad61b1f94fd8252e7a3c492e8355bcc45b74d
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-28 12:45:04 +0200

    delete bottom padding, use Slider for desktop unstead RequestList

--- sections/Requests/Requests.tsx
+++ sections/Requests/Requests.tsx
@@ -1,13 +1,12 @@
 'use client';
+
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
 
 import classNames from 'classnames';
 
 import { Section } from '@/components/Section';
 import { Container } from '@/components/Container';
 import { SectionTitle } from '@/components/SectionTitle';
-import { RequestsList } from '@/components/RequestsList';
 import { Slider } from '@/components/Slider';
 import { RequestCard } from '@/components/RequestCard';
 
@@ -19,31 +18,16 @@ export type IRequests = {
 
 export const Requests = ({ className = '' }: IRequests) => {
   const { title, requests } = dataJson;
-  const isDesktop = useMediaQuery({
-    query: '(min-width: 1280px)',
-  });
 
   return (
-    <Section
-      className={classNames('', className)}
-      isBottomPadding={true}
-      isTopPadding={true}
-    >
+    <Section className={classNames('', className)} isTopPadding={true}>
       <Container>
         <SectionTitle
           text={title}
           center={false}
           className="mb-7 xl:mb-[60px]"
         />
-        {isDesktop ? (
-          <RequestsList requestsList={requests} />
-        ) : (
-          <Slider
-            section={'requests'}
-            data={requests}
-            component={RequestCard}
-          />
-        )}
+        <Slider section={'requests'} data={requests} component={RequestCard} />
       </Container>
     </Section>
   );
