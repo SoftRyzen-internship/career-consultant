354cb8a642a65ca4a952409d01a10459c4b18e17
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-03-29 14:53:16 +0000

    fix #11 bug & changed services card mb & changed card duration gap

--- components/CardTitle/CardTitle.tsx
+++ components/CardTitle/CardTitle.tsx
@@ -9,7 +9,7 @@ export const CardTitle = ({ text, className = '' }: ICardTitle) => {
   return (
     <h3
       className={classNames(
-        'text-xl font-fixel mr-auto xl:text-3xl text-text01',
+        'text-xl font-fixel mr-auto xl:text-3xl text-text01 xl:font-medium xl:leading-[1.15]',
         className,
       )}
     >
